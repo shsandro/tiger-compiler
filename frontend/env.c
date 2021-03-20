7b0935a9af50b68eaf8f4e7d4575dd7e52f1d055
shsandro <shsandro@github.com>
2021-03-20 20:06:29 -0300

    update env.h env.c

--- frontend/env.c
+++ frontend/env.c
@@ -6,16 +6,20 @@
 #include "include/types.h"
 #include "include/util.h"
 
-E_enventry E_VarEntry(Ty_ty ty) {
+E_enventry E_VarEntry(Tr_access access, Ty_ty ty) {
     E_enventry e = checked_malloc(sizeof(*e));
     e->kind = E_varEntry;
+    e->u.var.access = access;
     e->u.var.ty = ty;
     return e;
 }
 
-E_enventry E_FunEntry(Ty_tyList formals, Ty_ty result) {
+E_enventry E_FunEntry(Tr_level level, Temp_label label, Ty_tyList formals,
+                      Ty_ty result) {
     E_enventry e = checked_malloc(sizeof(*e));
     e->kind = E_funEntry;
+    e->u.fun.level = level;
+    e->u.fun.label = label;
     e->u.fun.formals = formals;
     e->u.fun.result = result;
     return e;
@@ -31,26 +35,41 @@ S_table E_base_tenv(void) {
     return tenv;
 }
 
-#define ORD "ord"
-#define GETCHAR "getchar"
-#define CHR "chr"
-#define PRINT "print"
-#define PRINTI "printi"
-#define END ""
-
-char* externalCallNames[] = {ORD, GETCHAR, CHR, PRINT, PRINTI, END};
-
 S_table E_base_venv(void) {
     S_table venv = S_empty();
-    S_enter(venv, S_Symbol(ORD),
-            E_FunEntry(Ty_TyList(Ty_String(), NULL), Ty_Int()));
-    S_enter(venv, S_Symbol(GETCHAR), E_FunEntry(NULL, Ty_String()));
-    S_enter(venv, S_Symbol(CHR),
-            E_FunEntry(Ty_TyList(Ty_Int(), NULL), Ty_String()));
-    S_enter(venv, S_Symbol(PRINT),
-            E_FunEntry(Ty_TyList(Ty_String(), NULL), Ty_Void()));
-    S_enter(venv, S_Symbol(PRINTI),
-            E_FunEntry(Ty_TyList(Ty_Int(), NULL), Ty_Void()));
+
+    S_enter(venv, S_Symbol("print"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(),
+                       Ty_TyList(Ty_String(), NULL), Ty_Void()));
+    S_enter(venv, S_Symbol("flush"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(), NULL, Ty_Void()));
+    S_enter(venv, S_Symbol("getchar"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(), NULL, Ty_String()));
+    S_enter(venv, S_Symbol("ord"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(),
+                       Ty_TyList(Ty_String(), NULL), Ty_Int()));
+    S_enter(venv, S_Symbol("chr"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(),
+                       Ty_TyList(Ty_Int(), NULL), Ty_String()));
+    S_enter(venv, S_Symbol("size"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(),
+                       Ty_TyList(Ty_String(), NULL), Ty_Int()));
+    S_enter(
+        venv, S_Symbol("substring"),
+        E_FunEntry(Tr_outermost(), Temp_newlabel(),
+                   Ty_TyList(Ty_String(),
+                             Ty_TyList(Ty_Int(), Ty_TyList(Ty_Int(), NULL))),
+                   Ty_String()));
+    S_enter(venv, S_Symbol("concat"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(),
+                       Ty_TyList(Ty_String(), Ty_TyList(Ty_String(), NULL)),
+                       Ty_String()));
+    S_enter(venv, S_Symbol("not"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(),
+                       Ty_TyList(Ty_Int(), NULL), Ty_Int()));
+    S_enter(venv, S_Symbol("exit"),
+            E_FunEntry(Tr_outermost(), Temp_newlabel(),
+                       Ty_TyList(Ty_Int(), NULL), Ty_Void()));
 
     return venv;
 }
--- frontend/include/env.h
+++ frontend/include/env.h
@@ -1,6 +1,7 @@
 #ifndef ENV_H
 #define ENV_H
 
+#include "translate.h"
 #include "types.h"
 
 typedef struct E_enventry_ *E_enventry;
@@ -9,17 +10,21 @@ struct E_enventry_ {
     enum { E_varEntry, E_funEntry } kind;
     union {
         struct {
+            Tr_access access;
             Ty_ty ty;
         } var;
         struct {
+            Tr_level level;
+            Temp_label label;
             Ty_tyList formals;
             Ty_ty result;
         } fun;
     } u;
 };
 
-E_enventry E_VarEntry(Ty_ty ty);
-E_enventry E_FunEntry(Ty_tyList formals, Ty_ty result);
+E_enventry E_VarEntry(Tr_access access, Ty_ty ty);
+E_enventry E_FunEntry(Tr_level level, Temp_label label, Ty_tyList formals,
+                      Ty_ty results);
 
 S_table E_base_tenv(void); /* contains predefined functions */
 S_table E_base_venv(void);
